d143e24c4626319f46ca2acb54e429611b0b0bcf
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2026-04-07 19:49:24 -0400

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    HashSet<Integer> set = new HashSet<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for (int i = 0; i < numCourses; i++){
+            map.put(i, new ArrayList<>());
+        }
+
+        for (int[] pre: prerequisites){
+            map.get(pre[0]).add(pre[1]);
+        }
+
+        for (int i = 0; i < numCourses; i++){
+            if (!dfs(i)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int c){
+        if (set.contains(c)) return false;
+
+        if (map.get(c).isEmpty()) return true;
+
+        set.add(c);
+        for (int pre: map.get(c)){
+            if (!dfs(pre)) return false;
+        }
+        set.remove(c);
+        map.put(c, new ArrayList<>());
+        return true;
+    }
+}
