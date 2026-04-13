1641fc266311aa76fc38f60c8a56129f49f6705d
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2026-04-13 14:45:47 -0400

    Add: course-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-11.java
@@ -0,0 +1,39 @@
+class Solution {
+    List<List<Integer>> adj;
+    List<Integer> output = new ArrayList<>();
+    int[] indegree;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        indegree = new int[numCourses];
+        for (int[] pre: prerequisites){
+            indegree[pre[0]]++;
+            adj.get(pre[1]).add(pre[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++){
+            if (indegree[i] == 0){
+                dfs(i);
+            }
+        }
+
+        if (output.size() != numCourses) return new int[0];
+        int[] res = new int[output.size()];
+        for (int i = 0; i < output.size(); i++){
+            res[i] = output.get(i);
+        }
+        return res;      
+    }
+
+    void dfs(int node){
+        output.add(node);
+        indegree[node]--;
+        for (int nei: adj.get(node)){
+            indegree[nei]--;
+            if (indegree[nei] == 0) dfs(nei);
+        }
+    }
+}
