03a8031ea4026e86dfcfb348616ebd1add44fc14
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2026-05-12 20:58:28 -0400

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,32 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int val = stack.pop();
+        if (!minStack.isEmpty() && minStack.peek().equals(val)){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
